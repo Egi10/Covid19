736db4d70d4e72461997474eb12c43fde2c83cb9
wowbid <egi@wowbid.live>
2020-04-15 18:20:04 +0700

    add bottom navigation

---
+++ app/src/main/java/id/buaja/covid19/ui/main/MainActivity.kt
@@ -0,0 +1,32 @@
+package id.buaja.covid19.ui.main
+
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import id.buaja.covid19.R
+import id.buaja.covid19.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun contentView(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun initObservable() {}
+
+    override fun initView() {
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
