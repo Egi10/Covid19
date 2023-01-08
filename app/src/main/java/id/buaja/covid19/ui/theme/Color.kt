f34c8842e995ad2749594283969e3ce5d5f9a063
Julsapargi Nursam <julsapargi.nursam@efishery.com>
2023-01-08 12:26:29 +0700

    feat(theme) add theme compose

---
+++ app/src/main/java/id/buaja/covid19/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package id.buaja.covid19.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BlueGrey = Color(0xFF607d8b)
+val MineralGreen = Color(0xFF455a64)
+val Wattle = Color(0xFFcddc39)
+val DarkJungleGreen = Color(0xFF212121)
+val Boulder = Color(0xFF757575)
+val LavenderPinocchio = Color(0xFFE0E0E0)
---
+++ app/src/main/java/id/buaja/covid19/ui/theme/Theme.kt
@@ -0,0 +1,22 @@
+package id.buaja.covid19.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val LightColorPalette = lightColors(
+    primary = BlueGrey,
+    primaryVariant = MineralGreen,
+)
+
+@Composable
+fun CovidTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = LightColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
